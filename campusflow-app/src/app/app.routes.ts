15aae90679b8270b3c17574ee00a6aecf60ff5c9
Halosesparta <u202211674@upc.edu.pe>
2025-06-23 17:58:43 -0500

    Configuración de rutas AUTH

--- campusflow-app/src/app/app.routes.ts
+++ campusflow-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegistroEstudianteComponent } from './components/auth/registro-estudiante/registro-estudiante.component';
+import { RegistroProfesorComponent } from './components/auth/registro-profesor/registro-profesor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registrar-estudiante', component: RegistroEstudianteComponent },
+  { path: 'registrar-profesor', component: RegistroProfesorComponent }
+];
---
+++ campusflow-app/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatIconModule, MatButtonModule, MatTooltipModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
