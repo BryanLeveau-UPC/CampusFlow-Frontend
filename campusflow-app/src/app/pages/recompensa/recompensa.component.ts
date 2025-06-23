bc345555b00d6fbbc1e9c52719c09eca08c8521c
Halosesparta <u202211674@upc.edu.pe>
2025-06-22 22:48:05 -0500

    Agregando todos los componentes

---
+++ campusflow-app/src/app/pages/recompensa/recompensa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recompensa',
+  imports: [],
+  templateUrl: './recompensa.component.html',
+  styleUrl: './recompensa.component.css'
+})
+export class RecompensaComponent {
+
+}
